6d5d64fc9ee3ddaee4a610b0daed8966955248a1
Johannes <johannes.besch@zg.ch>
2024-05-23 12:40:00 +0200

    several fixes: - whole package now in base - plyr:mapvalues removed as it does not return NA for values not found in look-up table, so changed this to base R - automatic level detection understands now that input var can also be values when division has been coded - direction.check now can disentangle division and labels - code and data to generate lookup table provided -warning messages improved

--- R/directioncheck.R
+++ R/directioncheck.R
@@ -5,9 +5,9 @@
 directioncheck <- function(to,vartype,var){
   if(to=="auto" & vartype%in%c("numeric","integer")){
     direction.to="labels"
-  }else if(to=="auto" & vartype=="character" & max(nchar(var))>1){
+  }else if(to=="auto" & vartype=="character" & any(grepl("[a-z]",var))){
     direction.to="values"
-  }else if(to=="auto" & vartype=="character" & max(nchar(var))==1){
+  }else if(to=="auto" & vartype=="character" & (max(nchar(var))==1)|all(grepl("[0-9]",var))){
     direction.to="labels"
   }
   return(direction.to)
