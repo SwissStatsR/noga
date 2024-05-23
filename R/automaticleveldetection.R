2e09a7983b7b384cec0affc22f6af44f3caf8f1b
Johannes <johannes.besch@zg.ch>
2024-05-23 15:32:50 +0200

    fixed issues and imports

--- R/automaticleveldetection.R
+++ R/automaticleveldetection.R
@@ -5,17 +5,14 @@
 automaticleveldetection <- function(var,language){
     if(any(grepl("[A-Za-z]", var) )==TRUE){
 
-      if(max(nchar(var))==1){noga.level <- "section"} else{
-        label.var <- paste0("name_",language)
-        lookup.filtered <- noga::lookup[noga::lookup[,eval(label.var)] %in% var,]
+      if(max(nchar(var))==1){noga.level <- "section"
+      } else{
+        lookup.filtered <- noga::lookup[eval(paste0("name_",language)) %in% var,]
         if(all(!is.na(lookup.filtered$type))==TRUE){noga.level <- "type"}
         if(all(!is.na(lookup.filtered$class))==TRUE & any(is.na(lookup.filtered$type))==TRUE){noga.level <- "class"}
         if(all(!is.na(lookup.filtered$group))==TRUE & any(is.na(lookup.filtered$class))==TRUE){noga.level <- "group"}
         if(all(!is.na(lookup.filtered$division))==TRUE & any(is.na(lookup.filtered$group))==TRUE){noga.level <- "division"}
-      }
-
-
-    }else{
+      } }else{
       noga.level <- switch(as.character(nchar(max(var,na.rm=TRUE))),
                            "1"="section",
                            "2"="division",
