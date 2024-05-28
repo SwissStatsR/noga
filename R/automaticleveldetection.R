63f14a3e47e93a89c85dc48d6ad7a1d2f987f478
Johannes <johannes.besch@zg.ch>
2024-05-28 10:53:20 +0200

    automatic change by force main branch

--- R/automaticleveldetection.R
+++ R/automaticleveldetection.R
@@ -3,29 +3,32 @@
 #' @noRd
 
 automaticleveldetection <- function(var,language){
-    if(any(grepl("[A-Za-z]", var) )==TRUE){
+  if(any(grepl("[A-Za-z]", var) )==TRUE){
 
-      if(max(nchar(var))==1){
-        noga.level <- "section"
-        } else{
-        label.var <- paste0("name_",language)
-        lookup.filtered <- noga::lookup[eval(label.var) %in% var,]
-        if(all(!is.na(lookup.filtered$type))==TRUE){noga.level <- "type"}
-        if(all(!is.na(lookup.filtered$class))==TRUE & any(is.na(lookup.filtered$type))==TRUE){noga.level <- "class"}
-        if(all(!is.na(lookup.filtered$group))==TRUE & any(is.na(lookup.filtered$class))==TRUE){noga.level <- "group"}
-        if(all(!is.na(lookup.filtered$division))==TRUE & any(is.na(lookup.filtered$group))==TRUE){noga.level <- "division"}
-      }
-
-
-    }else{
-      noga.level <- switch(as.character(nchar(max(var,na.rm=TRUE))),
-                           "1"="section",
-                           "2"="division",
-                           "3"="group",
-                           "4"="class",
-                           "6"="type",
-                           warning("Please provide the noga level manually, the automatic detection failed."))
+    if(max(nchar(var))==1){
+      noga.level <- "section"
+    } else{
+      label.var <- paste0("name_",language)
+      to.filter <- noga::lookup[eval(label.var)]
+      matched.successfully <- sapply(var, function(y) sapply(to.filter, function(x) grepl(y,x)))
+      matched.succesfully.index <- which(rowSums(matched.successfully)==1)
+      lookup.filtered <- noga::lookup[matched.succesfully.index,]
+      if(all(!is.na(lookup.filtered$type))==TRUE){noga.level <- "type"}
+      if(all(!is.na(lookup.filtered$class))==TRUE & any(is.na(lookup.filtered$type))==TRUE){noga.level <- "class"}
+      if(all(!is.na(lookup.filtered$group))==TRUE & any(is.na(lookup.filtered$class))==TRUE){noga.level <- "group"}
+      if(all(!is.na(lookup.filtered$division))==TRUE & any(is.na(lookup.filtered$group))==TRUE){noga.level <- "division"}
     }
+
+
+  }else{
+    noga.level <- switch(as.character(nchar(max(var,na.rm=TRUE))),
+                         "1"="section",
+                         "2"="division",
+                         "3"="group",
+                         "4"="class",
+                         "6"="type",
+                         warning("Please provide the noga level manually, the automatic detection failed."))
+  }
 
   return(noga.level)
 }
